390cebbf03b4fde958c850733af25e800ea20ecf
StellaZare <zareistella@uvic.ca>
2023-03-20 22:15:11 -0700

    monday developement

--- ThreeSum/ThreeSum.cpp
+++ ThreeSum/ThreeSum.cpp
@@ -4,31 +4,29 @@
 using namespace std;
 
 vector<vector<int>> threeSum(vector<int>& nums) {
-  vector<vector<int>> v;
+    vector<vector<int>> v;
+    sort(v.begin(), v.end());
     return v;    
 }
 
 void printVector(vector<int> v){
-  cout << "[";
+    cout << "[";
   
-  for(int n : v){
-    cout << n << " ";
-  }
-  cout << "]\n";
+    for(int n : v){
+        cout << n << " ";
+    }
+    cout << "]\n";
 }
 
 void printNestedVector(vector<vector<int>> vn){
-  for(vector<int> v : vn){
-    printVector(v);
-  }
+    for(vector<int> v : vn){
+        printVector(v);
+    }
 }
 
 int main(){
+    vector<int> list1 = {-1,0,1,2,-1,-4};
+    vector<vector<int>> listNested = {list1, list1};
 
-  vector<int> list1 = {-1,0,1,2,-1,-4};
-  vector<vector<int>> listNested = {list1, list1};
-  
-  printVector(list1);
-  //printNestedVector(listNested);
-  return 0;
+    return 0;
 }
