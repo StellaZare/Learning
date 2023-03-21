268d3e664ea2f3e8acb540a862bc07d20cbf0a76
StellaZare <zareistella@uvic.ca>
2023-03-20 19:48:47 -0700

    adds helper pritn functions

---
+++ ThreeSum/ThreeSum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+  vector<vector<int>> v;
+    return v;    
+}
+
+void printVector(vector<int> v){
+  cout << "[";
+  
+  for(int n : v){
+    cout << n << " ";
+  }
+  cout << "]\n";
+}
+
+void printNestedVector(vector<vector<int>> vn){
+  for(vector<int> v : vn){
+    printVector(v);
+  }
+}
+
+int main(){
+
+  vector<int> list1 = {-1,0,1,2,-1,-4};
+  vector<vector<int>> listNested = {list1, list1};
+  
+  printVector(list1);
+  //printNestedVector(listNested);
+  return 0;
+}
