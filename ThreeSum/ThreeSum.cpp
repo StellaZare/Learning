77dd0a127ac42e84201ae3678d4815b0206df14a
StellaZare <zareistella@uvic.ca>
2023-03-21 16:40:39 -0700

    implementation for ThreeSum

--- ThreeSum/ThreeSum.cpp
+++ ThreeSum/ThreeSum.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
-
-vector<vector<int>> threeSum(vector<int>& nums) {
-    vector<vector<int>> v;
-    sort(v.begin(), v.end());
-    return v;    
-}
 
 void printVector(vector<int> v){
     cout << "[";
@@ -24,9 +19,40 @@ void printNestedVector(vector<vector<int>> vn){
     }
 }
 
+vector<vector<int>> threeSum(vector<int>& nums) {
+    set<vector<int>> v;
+    sort(nums.begin(), nums.end());
+
+    for(int currIdx = 0; currIdx < nums.size(); currIdx++){
+        int leftIdx = currIdx + 1;
+        int rightIdx = nums.size() - 1;
+
+        while(leftIdx <  rightIdx){
+            int sum = nums[currIdx] + nums[leftIdx] + nums[rightIdx];
+            if (sum == 0){
+                vector<int> tuple = {nums[currIdx], nums[leftIdx], nums[rightIdx]};
+                v.insert(tuple);
+                leftIdx++;
+                rightIdx--;
+            }
+            else if(sum < 0){
+                leftIdx++;
+            }else{
+                rightIdx--;
+            }
+        }
+    }
+    vector<vector<int>> output;
+    for(vector<int> tuple :v){
+        output.push_back(tuple);
+    }
+    return output;    
+}
+
 int main(){
     vector<int> list1 = {-1,0,1,2,-1,-4};
-    vector<vector<int>> listNested = {list1, list1};
+    vector<vector<int>> listNested = threeSum(list1);
 
+    printNestedVector(listNested);
     return 0;
 }
